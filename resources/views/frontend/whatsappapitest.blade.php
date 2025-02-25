8892929ee277e8577f7d442c5251cb310e92c2eb
rajtiwariyng <rajtiwariyng@gmail.com>
2025-02-25 20:23:13 +0530

    whatsapp testing

---
+++ resources/views/frontend/whatsappapitest.blade.php
@@ -0,0 +1,36 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://cloudapi.wbbox.in/api/v1.0/messages/send-template/917838357872',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+  "messaging_product": "whatsapp",
+  "recipient_type": "individual",
+  "to": "919319927634",
+  "type": "template",
+  "template": {
+    "name": "purchaseorder",
+    "language": {
+      "code": "en"
+    },
+    "components": []
+  }
+}',
+  CURLOPT_HTTPHEADER => array(
+    'Authorization: Bearer dYjPebs9fU67IeafcZbL6g',
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
